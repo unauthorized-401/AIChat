5e2fecaeb4cb106e3d711e12a8d83ea4999d977a
Jiwon <moonlight_youlike@naver.com>
2023-12-19 21:38:19 +0900

    feat: apply swagger

---
+++ src/main/java/com/chat/with/ai/AIChat/configuration/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.chat.with.ai.AIChat.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "AI 채팅 API 명세서",
+                description = "AI 메신저를 위한 인터페이스 제공",
+                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {
+                "/common/user/join",            // 회원가입
+                "/common/user/login",           // 로그인
+                "/common/user/logout",          // 로그아웃
+                "/common/chat/request",         // 채팅 보내기
+                "/common/chat/response",        // 답변 받기
+        };
+
+        return GroupedOpenApi.builder()
+                .group("AIChat API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
