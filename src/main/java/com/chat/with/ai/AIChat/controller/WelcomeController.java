3000c13bb0608c7b3aa4786bcfb463d890b923d8
Jiwon <moonlight_youlike@naver.com>
2023-12-22 10:08:09 +0900

    refactor: remove unnecessary import

--- src/main/java/com/chat/with/ai/AIChat/controller/WelcomeController.java
+++ src/main/java/com/chat/with/ai/AIChat/controller/WelcomeController.java
@@ -1,9 +1,6 @@
 package com.chat.with.ai.AIChat.controller;
 
-import com.chat.with.ai.AIChat.controller.parameter.MessageParam;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
