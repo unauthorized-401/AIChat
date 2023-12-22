4c7a135fc4b664f0d65f9b40890aabfbecb55896
Jiwon <moonlight_youlike@naver.com>
2023-12-22 10:34:48 +0900

    feat: add devtools for develop html

--- src/main/java/com/chat/with/ai/AIChat/controller/WelcomeController.java
+++ src/main/java/com/chat/with/ai/AIChat/controller/WelcomeController.java
@@ -8,16 +8,19 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class WelcomeController {
 
+    // Login Page
     @GetMapping(value="/")
     public String login() {
         return "login";
     }
 
+    // Sign Up Page
     @GetMapping(value="/sign_up")
     public String sign_up() {
         return "sign_up";
     }
 
+    // AI Chat Page
     @GetMapping(value="/chat")
     public String chat() {
         return "chat";
